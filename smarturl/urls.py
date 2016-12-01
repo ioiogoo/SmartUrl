73ade183fc92880b8ac00dc3b258edf83f8ba810
ioiogoo <ioiogoo@qq.com>
2016-12-01 10:44:23 +0800

    add static config

--- smarturl/urls.py
+++ smarturl/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from main.views import index, get_url
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index),
     url(r'^(\w+)/$', get_url)
 ]
+
+
+if not settings.DEBUG:
+    urlpatterns += [url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})]
+
